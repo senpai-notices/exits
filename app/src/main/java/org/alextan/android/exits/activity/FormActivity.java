dd700a4af1762f024dc8606316d2bfb2f52d2048
tanman- <tanman-@users.noreply.github.com>
2016-10-14 17:43:01 +1100

    Cleanup

--- app/src/main/java/org/alextan/android/exits/activity/FormActivity.java
+++ app/src/main/java/org/alextan/android/exits/activity/FormActivity.java
@@ -37,7 +37,6 @@ import org.alextan.android.exits.service.GtfsService;
 import org.alextan.android.exits.util.LocationMath;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
